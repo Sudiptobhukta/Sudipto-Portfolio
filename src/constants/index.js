d862a1e8190de24de37216d728c6a920d27dcfd7
Sudiptobhukta <sudipto11bhukta@gmail.com>
2024-07-24 18:31:39 +0530

    Changes on the README, js and index.html

--- src/constants/index.js
+++ src/constants/index.js
@@ -9,63 +9,50 @@ export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer w
 
 export const EXPERIENCES = [
   {
-    year: "2023 - Present",
-    role: "Senior Full Stack Developer",
-    company: "Google Inc.",
-    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
-    technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
-  },
-  {
-    year: "2022 - 2023",
-    role: "Frontend Developer",
-    company: "Adobe",
-    description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
-    technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
+    year: "2023 - 2023",
+    role: "Python Developer",
+    company: "CodSoft",
+    description: `Build web applications using Djanog framework, Python and PostgreSQL. Integrated with PostgreSQL databases. Collaborated with stakeholders to define project requirements and timelines.`,
+    technologies: ["HTML", "CSS" , "Django", "Python", "PostgreSQL"],
   },
   {
     year: "2021 - 2022",
-    role: "Full Stack Developer",
-    company: "Facebook",
-    description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
-    technologies: ["Python", "Svelte", "Three.js", "Postgres"],
+    role: "Software Developer",
+    company: "GAOTek.Inc",
+    description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
+    technologies: ["HTML", "CSS", "mySQL", "React"],
   },
-  {
-    year: "2020 - 2021",
-    role: "Software Engineer",
-    company: "Paypal",
-    description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
-    technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
-  },
+ 
 ];
 
 export const PROJECTS = [
   {
-    title: "E-Commerce Website",
+    title: "ERP Portal",
     image: project1,
     description:
-      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
-    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
-  },
-  {
-    title: "Task Management App",
-    image: project2,
-    description:
-      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
-    technologies: ["HTML", "CSS", "Angular", "Firebase"],
+      "A fully functional ERP website for college with features like adding time table, student login, teachers login and user authentication.",
+    technologies: ["HTML", "CSS" , "Django", "Python", "PostgreSQL"],
   },
   {
     title: "Portfolio Website",
     image: project3,
     description:
       "A personal portfolio website showcasing projects, skills, and contact information.",
-    technologies: ["HTML", "CSS", "React", "Bootstrap"],
+    technologies: ["HTML", "CSS", "React", "Tailwind CSS" ,"javaScript"],
   },
   {
-    title: "Blogging Platform",
+    title: "Sentiment Analysis",
+    image: project2,
+    description:
+      "An application for managing Sentiment of the text,it uses API for extracting movies text and giving Sentiment in Emoji.",
+    technologies: ["Python", "Matplotlib","Seaborn"],
+  },
+  {
+    title: "Mr. Jumpy",
     image: project4,
     description:
-      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
-    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
+      "A platform to play a simple yet addicted game, made with Python, Pygame, graphics, pymusic",
+    technologies: ["Python","Pygame","Pymusic"],
   },
 ];
 
